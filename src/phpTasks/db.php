b8ca93c41f92c471e8fa4113a7d94051531c3246
Damith <amk1012409@student.hamk.fi>
2026-02-10 14:02:03 +0200

    set connection string

--- src/phpTasks/db.php
+++ src/phpTasks/db.php
@@ -5,10 +5,12 @@ $password = "JfoE7lkm";
 $dbname = "wp_amk1012409";
 
 //connection string
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password,$dbname );
 
 // test connection 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+echo "Connected successfully";
+$conn->close();
 ?>
